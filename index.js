5da753d7b20933d7b60bbc3689b6ed3eed216931
Ayushkumar8969 <ayushkumar777889@gmail.com>
2025-06-28 16:46:50 +0530

    get-result-method

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const http  = require('http');// use http module
-const fs = require('fs/promises');
-require('dotenv').config();
+const http = require("http"); // use http module
+const fs = require("fs/promises");
+require("dotenv").config();
 //process.env
 const PORT = 8000;
 console.log("hii");
-const server = http.createServer(async (req,res)=>{
+const server = http.createServer(async (req, res) => {
   // if(req.url == "/home"){
   //   let data =await fs.readFile('./homePage.html',{encoding:'utf-8'});
   //   res.end(data);
@@ -16,29 +16,34 @@ const server = http.createServer(async (req,res)=>{
   //   return res.end('No Page found');
   // }
 
-  let body = '';
-  req.on('data',(chunk)=>{
-    body = body+chunk;
+  let body = "";
+  req.on("data", (chunk) => {
+    body = body + chunk;
   });
 
-  req.on('end',()=>{
-      console.log('data recieived');
-      console.log(body);
-      //read and write it will send the data
-      return res.end('data recieved');
+  req.on("end", () => {
+    console.log("data recieived");
+    console.log(body);
+    //read and write it will send the data
+    return res.end("data recieved");
   });
 });
-function justSayHello(){
-  console.log('hello');
-  console.log('hi');
+function justSayHello() {
+  console.log("hello");
+  console.log("hi");
 }
 
-server.listen(PORT,(err) =>{
-  if(err){
-    console.log('error',err);
+// creating some method
+function getResult() {
+  console.log("proccessing the result");
+}
+
+server.listen(PORT, (err) => {
+  if (err) {
+    console.log("error", err);
     return;
   }
-  console.log("My Server has started At PORT",PORT);
+  console.log("My Server has started At PORT", PORT);
 });
 
 //localhost 127.0.0.1
