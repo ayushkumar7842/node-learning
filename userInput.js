c9a3b748dfea54a3d4bdb16f0e788a4778a20839
Ayushkumar8969 <ayushkumar777889@gmail.com>
2025-06-28 15:28:01 +0530

    third-input

--- userInput.js
+++ userInput.js
@@ -1,15 +1,17 @@
-//shell 
-const readline = require('readline');
+//shell
+const readline = require("readline");
 
 const r1 = readline.createInterface({
-    input:process.stdin,
-    output:process.stdout
+  input: process.stdin,
+  output: process.stdout,
 });
 
-r1.question('Enter the first number',(num1)=>{
-    r1.question('Enter the second number',(num2)=>{
-        const sum = Number(num1)+ Number(num2);
-        console.log('the sum is',sum);
-        r1.close();
-    })
-})
+r1.question("Enter the first number: ", (num1) => {
+  r1.question("Enter the second number: ", (num2) => {
+    r1.question("Enter the third number: ", (num3) => {
+      const sum = Number(num1) + Number(num2) + Number(num3);
+      console.log("the sum is", sum);
+      r1.close();
+    });
+  });
+});
